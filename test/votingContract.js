da9434a9d1743693a7baea1299d51d393d7437e2
Your Name <you@example.com>
2024-03-05 19:08:03 -0600

    Added functionality for the client side access, few configuration files added with some changes in code at voting contract.

--- test/votingContract.js
+++ test/votingContract.js
@@ -1,8 +1,13 @@
 /** @format */
 
+const { assert } = require("chai");
+
 var VotingContract = artifacts.require("./VotingContract.sol");
 
 contract("VotingContract", function (accounts) {
+  //this is accessible for all the tests
+  let votingContractInstance;
+
   it("initailizes with two candidates", function () {
     return VotingContract.deployed()
       .then(function (instance) {
@@ -20,6 +25,33 @@ contract("VotingContract", function (accounts) {
       })
       .then(function (count) {
         assert.equal(count, 0);
+      });
+  });
+
+  it("initailizes candidates with correct attributes", function () {
+    return VotingContract.deployed()
+      .then(function (instance) {
+        votingContractInstance = instance;
+        return votingContractInstance.candidateDetails(1);
+      })
+      .then(function (candidateDetail) {
+        assert.equal(candidateDetail[0], 1, "contains the correct id");
+        assert.equal(candidateDetail[1], "John", "contains the correct name");
+        assert.equal(candidateDetail[2], 0, "contains correct number of votes");
+        return votingContractInstance.candidateDetails(2);
+      })
+      .then(function (candidateDetail) {
+        assert.equal(candidateDetail[0], 2, "contains the correct id");
+        assert.equal(
+          candidateDetail[1],
+          "Micheal",
+          "contains the correct name"
+        );
+        assert.equal(
+          candidateDetail[2],
+          0,
+          "contains the corret number of votes"
+        );
       });
   });
 });
